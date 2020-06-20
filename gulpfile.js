b62b4f5697be6ee5765ce4c38e0780b57ba5d9cc
Serge Bast <serge.bast@aol.com>
2020-06-20 18:57:05 +0500

    chore(gulpfile.js): Стартовать сервер и отслеживать изменения кода

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,19 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const watch = require('gulp-watch');
+
+gulp.task('watch', function () {
+  watch(['./src/*.html', './src/**/*.css'], gulp.parallel(browserSync.reload));
+});
+
+gulp.task('server', function () {
+  browserSync.init({
+    server: {
+      baseDir: './src/',
+    },
+  });
+});
+
+gulp.task('default', function () {
+  gulp.parallel('server', 'watch');
+});
